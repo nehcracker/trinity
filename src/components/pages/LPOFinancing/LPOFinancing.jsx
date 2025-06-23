9ba79a58dfbe643c24a4cbe81ddc9e0305bfae01
nehcracker <nehlmac4@gmail.com>
2025-06-23 21:56:11 +0300

    SEO update

--- src/components/pages/LPOFinancing/LPOFinancing.jsx
+++ src/components/pages/LPOFinancing/LPOFinancing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HelmetProvider  } from 'react-helmet-async';
+import { Helmet } from "react-helmet";
 import styles from './LPOFinancing.module.css';
 
 // Import section components
@@ -14,14 +14,14 @@ import CTASection from './CTASection/CTASection';
 const LPOFinancing = () => {
   return (
     <div className={styles.container}>
-      <HelmetProvider >
+      <Helmet >
         <title>LPO Financing | Capital Financing | Business line of credit</title>
         <meta name="description" content="Explore our LPO Financing solutions to support your business growth with flexible and reliable financing options." />
         <meta name="keywords" content="LPO Financing, Business Loans, Purchase Order Financing, Business line of credit, Contract financing, contract financing, Commercial business loan, Purchase order, LPO financing, Business financing, Business capital loans, Trinity" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:image" content="https://trinityfinancing.com/micro-finance.jpg" />
         <link rel="canonical" href="https://trinityfinancing.com/lpofinancing/" />
-      </HelmetProvider >
+      </Helmet >
       
       {/* Main content sections */}
       <HeroSectionLPO />
